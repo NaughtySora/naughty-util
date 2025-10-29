4ef18dfdf771f2b3094d71a982c86149ae639e6c
Trofymenko Vladyslav <naughtysora@proton.me>
2025-10-29 19:18:04 +0200

    object iterator

--- types/iterator.d.ts
+++ types/iterator.d.ts
@@ -3,4 +3,9 @@ import { UtilsMisc } from "./misc";
 export interface UtilsIterator extends Pick<UtilsMisc, "enumerate" | "range"> {
   pick<T extends object, K extends keyof T>(sequence: Iterable<T>, name: K): Generator<T[K]>;
   limit<T>(sequence: Iterable<T>, limit: number): Generator<T>;
+  object: {
+    keys<O extends object>(obj: O): Generator<string>;
+    values<O extends object>(obj: O): Generator<any>;
+    entries<O extends object>(obj: O): Generator<[string, any]>;
+  }
 }
