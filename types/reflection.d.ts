3d19fe466fcd0197f14f2fcd081cdef7f4718a2e
sora <naughtysora@proton.me>
2025-11-21 12:54:34 +0200

    reflection types

--- types/reflection.d.ts
+++ types/reflection.d.ts
@@ -1,13 +1,22 @@
 import { UtilsArray } from "./array";
 
+type NonNullablePrimitive = string | number | boolean |
+  bigint | symbol | undefined;
+type Falsy = false | 0 | -0 | 0n | "" | null | undefined | typeof NaN;
+interface ArrayLike {
+  length: number;
+  [n: number]: never;
+}
+
 export interface UtilsReflection {
-  isClass(entity: any): boolean;
-  isEmpty(entity: any): boolean;
-  isPrimitive(entity: any): boolean;
-  isComplex(entity: any): boolean;
-  isFalsy(entity: any): boolean;
-  isError(entity: any): boolean;
-  isAsyncFunction(entity: any): boolean;
-  isObject(entity: any): boolean;
+  isClass<C extends new (...args: any[]) => any>(entity: any): entity is C;
+  isEmpty<E extends (null | undefined)>(entity: any): entity is E;
+  isPrimitive(entity: any): entity is NonNullablePrimitive;
+  isComplex(entity: any): entity is (object | Function);
+  isFalsy<T extends any>(entity: T): entity is Exclude<T, Falsy>;
+  isError(entity: any): entity is InstanceType<typeof Error>;
+  isAsyncFunction<A extends (...args: any[]) => Promise<any>>(entity: any): entity is A;
+  isObject(entity: any): entity is ((object & ArrayLike) | Function);
   isArray: UtilsArray["valid"];
 }
+
